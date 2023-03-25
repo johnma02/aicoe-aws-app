b185c579ead765983ec266137d252793d2628172
johnma <ma.jonathan02@gmail.com>
2023-03-25 15:02:01 -0400

    added CodeBuild integration

---
+++ aicoe-app/src/components/map.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+import styles from '@/styles/Home.module.css' 
+export function map(): JSX.Element{
+    return <div>
+{/* store 10 day forecast in mongo atlas, route onClick calls to REST retreive operations */}
+
+    </div>
+}
