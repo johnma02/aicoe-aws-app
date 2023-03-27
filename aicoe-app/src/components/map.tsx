2d256a212de61ce49c70bcde4470b4ef0597242c
johnma <ma.jonathan02@gmail.com>
2023-03-27 14:10:13 -0400

    Switched to Google Maps API, created map component, switched to global light theme

--- aicoe-app/src/components/map.tsx
+++ aicoe-app/src/components/map.tsx
@@ -1,8 +1,43 @@
-import React from "react"
-import styles from '@/styles/Home.module.css' 
-export function map(): JSX.Element{
-    return <div>
-{/* store 10 day forecast in mongo atlas, route onClick calls to REST retreive operations */}
+import { GoogleMap, useLoadScript } from '@react-google-maps/api';
+import { useMemo } from 'react';
+import styles from '@/styles/Home.module.css';
 
-    </div>
+interface MapProps{
+    lattitude: number;
+    longitude: number;
+    zoom: number;
+}
+
+export default function Map({lattitude, longitude, zoom}: MapProps):JSX.Element {
+    const mapCenter = useMemo(
+        () => ({ lat:lattitude, lng: longitude }),
+        []
+    );
+    const mapOptions = useMemo<google.maps.MapOptions>(
+        () => ({
+            clickableIcons: true,
+        }),
+        []
+    );
+    const { isLoaded } = useLoadScript({
+        googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY as string,
+    });
+    if (!isLoaded) {
+        return <div>Google Maps is loading...</div>;
+    }
+    console.log("FINDING: "+JSON.stringify(process.env));
+    return (
+        <div className={styles.homeWrapper}>
+            <div className={styles.sidebar}>
+            </div>
+            <GoogleMap
+                options={mapOptions}
+                zoom={zoom}
+                center={mapCenter}
+                mapTypeId={google.maps.MapTypeId.ROADMAP}
+                mapContainerStyle={{ width: '800px', height: '800px' }}
+                onLoad={() => console.log('Google Maps component loaded')}
+            />
+        </div>
+    );
 }
