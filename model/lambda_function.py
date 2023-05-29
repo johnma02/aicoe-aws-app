3ec76cc3a1da1f3531d024038a8211ac32ae1f59
johnma <ma.jonathan02@gmail.com>
2023-05-29 16:59:21 -0400

    feat(model): add model for CodeBuild image build

---
+++ model/lambda_function.py
@@ -0,0 +1,290 @@
+from nwm_predict import *
+import netCDF4 as nc
+import boto3
+
+
+def lambda_handler(event, context):
+    start = time.time()
+
+    print('Predict the risk level of daily EOF runoff:')
+
+    current_path = os.getcwd()
+
+    # Step 1: prepare the input files
+    # read the hourly value from the netcdf file for n days in a row
+
+    # Set the new directory path
+    new_path = "./data/"
+    start_date = datetime.date(2008, 2, 1)
+    end_date = datetime.date(2008, 2, 1)
+
+    land_output = [
+        f"{new_path}/{date.strftime('%Y%m%d')}.LDASOUT_DOMAIN1.nc" for date in
+        (start_date + datetime.timedelta(days=x)
+         for x in range((end_date - start_date).days + 1))
+    ]
+
+    runoff_output = [
+        f"{new_path}/{date.strftime('%Y%m%d')}.RTOUT_DOMAIN1.nc" for date in
+        (start_date + datetime.timedelta(days=x)
+         for x in range((end_date - start_date).days + 1))
+    ]
+
+    # Variables in daily Land file
+    # dict_keys(['ACSNOM', 'EMISSM', 'FIRA', 'FSA', 'IRB', 'LWFORC', 'Q2MVM', 'QSNOW', 'QSNOWM', 'RAINRATE', 'SOILICEM',
+    # 'SOILSAT', 'SOILSATM', 'SOIL_TM', 'SOIL_WM', 'SWFORC', 'T2MVM', 'TGBM', 'crs', 'time', 'x', 'y'])
+
+    # Variables in daily runoff file
+    # dict_keys(['QBDRYRT', 'QBDRYRT_2', 'QSTRMVOLRT', 'SFCHEADSUBRTM', 'SOIL_MM', 'ZWATTABLRTM', 'crs', 'qqsfc_acc', 'qqsub_acc', 'time', 'x', 'y'])
+
+    # Variables in selected_variables_by_cluster.nc
+    # dict_keys(['time', 'x', 'y', 'ACSNOM', 'QSNOW', 'ACCPRCP'])
+
+    # Change TGV to T2MVM, and remove SHG
+    # influential variables for each cluster
+    influential_vars = {
+        # 'RAINRATE', 'SFHD', 'ACSNOM', 'SOILSAT'
+        1: ['RAINRATE', 'SFCHEADSUBRTM', 'ACSNOM', 'SOILSATM'],
+        2: ['RAINRATE', 'ACSNOM', 'SOILSATM', 'T2MVM'],
+        3: ['RAINRATE', 'SOIL_WM_1', 'SOIL_MM_3', 'ACSNOM', 'SFCHEADSUBRTM'],
+        4: ['RAINRATE', 'SOIL_WM_1', 'FIRA', 'SFCHEADSUBRTM', 'SOIL_MM_4', 'ACSNOM'],
+        5: ['RAINRATE', 'ACSNOM', 'SFCHEADSUBRTM', 'T2MVM', 'SOILSATM', 'qqsub_acc', 'FIRA']
+    }
+
+    # runoff prediction for each cluster
+
+    file_path = current_path + '/trained_clusters/'
+
+    for i in range(1, 6):
+
+        # get the influential variables
+        # get() Method return the value for the given key if present in the dictionary.
+        x = influential_vars.get(i)
+        # Deep copy is a process in which the copying process occurs recursively.
+        y = deepcopy(x)
+        y.reverse()
+        variables = y
+
+    # get the hourly model output from WRF-Hydro model runs
+        daily_values = get_daily_values_from_nwm(
+            land_output, runoff_output, variables)
+
+        # Step 2: load the statistical models and make predictions
+        # provide the path to both event and runoff prediction models (saved as a classification model, clas.joblib.dat and a regression model, reg.joblib.dat)
+        mod_files = file_path + 'cluster' + str(i) + '/'
+
+        # predictions of occurrence and the associated probability of EOF runoff using prediction classification models, and
+        # predictions of its magnitudes using the regression model
+
+        predict_results = predict_runoff(mod_files, daily_values, variables)
+        # print(predict_results)
+        predict = np.array(predict_results.variables['EVENT'])
+        # save daily prediction results (event, probability and magnitude) into NetCDF format
+        inputs = current_path + '/preds/p' + str(i) + '.nc'
+        save_data_to_netCDF(inputs, predict_results)
+
+    # Extract the predictions of event, probability, and magnitude for each cluster
+    event_predictions = {}
+    magnitude_predictions = {}
+    probability_predictions = {}
+
+    # Path to prediction results
+    prediction_path = current_path + '/preds/'
+    # Path to cluster definition
+    cluster_file_path = current_path + '/preds/cluster_definition_1km.nc'
+
+    # Iterate through clusters
+    for cluster_number in range(1, 6):
+        # Predictions (event, probability, and magnitude) for each cluster
+        prediction_file = prediction_path + 'p' + str(cluster_number) + '.nc'
+        predictions = xr.open_dataset(prediction_file)
+
+        # Cluster definition
+        cluster_definition = xr.open_dataset(cluster_file_path)
+
+        # Event prediction
+        event_values = np.fliplr(predictions.EVENT.values)
+
+        # Probability prediction
+        probability_values = np.fliplr(predictions.PROB.values)
+
+        # Magnitude prediction
+        magnitude_values = np.fliplr(predictions.RUNOFF.values)
+
+        # Assign "1" to a specific cluster for each iteration
+        cluster_values = cluster_definition.Cluster.values
+        cluster_values[cluster_values != cluster_number] = np.nan
+        cluster_values[cluster_values == cluster_number] = 1
+        cluster_values_new_dim = cluster_values
+
+        # Extract the prediction from a given cluster
+        # re_cluster_values acts as a filter to mask out the values not in a given cluster
+        batch_size, num_rows, num_cols = event_values.shape
+        re_cluster_values = np.repeat(
+            cluster_values_new_dim[np.newaxis, :, :], batch_size, axis=0)
+
+        multi_event = np.multiply(event_values, re_cluster_values)
+        multi_magnitude = np.multiply(magnitude_values, re_cluster_values)
+        multi_probability = np.multiply(probability_values, re_cluster_values)
+
+        # Assign predictions to different variables
+        event_predictions[cluster_number] = multi_event
+        magnitude_predictions[cluster_number] = multi_magnitude
+        probability_predictions[cluster_number] = multi_probability
+
+    # Assign model predictions to event, probability, and magnitude
+    # Starting from cluster 1
+    event = event_predictions.get(1)
+    probability = probability_predictions.get(1)
+    magnitude = magnitude_predictions.get(1)
+
+    # Continue with clusters 2 to 5
+    for cluster_num in range(2, 6):
+        cluster_event = event_predictions.get(cluster_num)
+        is_nan_event = np.isnan(event)
+        event[is_nan_event] = cluster_event[is_nan_event]
+
+        cluster_probability = probability_predictions.get(cluster_num)
+        is_nan_probability = np.isnan(probability)
+        probability[is_nan_probability] = cluster_probability[is_nan_probability]
+
+        cluster_magnitude = magnitude_predictions.get(cluster_num)
+        is_nan_magnitude = np.isnan(magnitude)
+        magnitude[is_nan_magnitude] = cluster_magnitude[is_nan_magnitude]
+
+    event_copy = deepcopy(event)
+    probability_copy = deepcopy(probability)
+    magnitude_copy = deepcopy(magnitude)
+
+    # open an existing NetCDF template
+    pd = xr.open_dataset(current_path + '/preds/p4.nc')
+
+    pd.EVENT.values = event_copy
+    pd.RUNOFF.values = probability_copy
+    pd.PROB.values = magnitude_copy
+
+    # ef = pd.transpose('time', 'y', 'x')
+    ef = pd
+    ef.to_netcdf(current_path + '/preds/pF.nc')
+
+    end = time.time()
+
+    print("Time: {0:.4f}s".format(end - start))
+
+    # Risk level prediction based on the occurrence probability and magnitudes of daily runoff event using the risk level metrics
+
+    start = time.time()
+
+    print('Predict the risk level of daily EOF runoff based on the pre-defined risk matrix:')
+
+    current_path = os.getcwd()
+
+    # step 1: read the prediction for a given time period (10-day in our case)
+    input_directory = current_path + '/preds/'
+
+    # read the saved final output file with this name
+    file_path = input_directory + 'pF.nc'
+    x = xr.open_dataset(file_path)
+
+    # read the magnitudes of daily EOF runoff
+    y = x.RUNOFF.values
+    y_map = y
+
+    conditions = [
+        y < 0.1,
+        (y >= 0.1) & (y <= 0.45),
+        (y > 0.45) & (y <= 2.4),
+        (y > 2.4) & (y <= 11.6),
+        y > 11.6
+    ]
+
+    choices = [0, 1, 2, 3, 4]
+    y_map = np.select(conditions, choices, default=y_map)
+
+    risk_levels = np.full((1, 1601, 2001), np.nan)
+    # Convert the occurrence probability and categorical magnitude of runoff into risk levels (4 levels) using the risk-level metrics
+    for index, values in np.ndenumerate(x.PROB.values):
+        i, j, k = index
+        magnitude = y_map[i, j, k]
+
+        if values > 0.75:
+            if magnitude <= 1:
+                risk_levels[i, j, k] = 1
+            elif magnitude == 2 or magnitude == 3:
+                risk_levels[i, j, k] = 2
+            else:
+                risk_levels[i, j, k] = 3
+        elif 0.75 >= values > 0.5:
+            if magnitude <= 1:
+                risk_levels[i, j, k] = 1
+            else:
+                risk_levels[i, j, k] = 2
+        elif 0.5 >= values > 0.25:
+            if magnitude <= 1:
+                risk_levels[i, j, k] = 1
+            else:
+                risk_levels[i, j, k] = 2
+        else:
+            if magnitude <= 1:
+                risk_levels[i, j, k] = 0
+            else:
+                risk_levels[i, j, k] = 1
+
+    # Copy 10-day risk level prediction to a new variable, risk_levels_copy
+    risk_levels_copy = deepcopy(risk_levels)
+
+    # min, med and max risk level for each grid (1km x 1km) over a given period
+    median_risk_levels = np.empty((1, 1601, 2001)) * np.nan
+    min_risk_levels = np.empty((1, 1601, 2001)) * np.nan
+    max_risk_levels = np.empty((1, 1601, 2001)) * np.nan
+
+    num_periods, num_rows, num_cols = risk_levels_copy.shape
+
+    for index, values in np.ndenumerate(risk_levels_copy[0]):
+        i, j = index
+        risk_levels_list = []
+        for k in range(num_periods):
+            risk_levels_list.append(risk_levels_copy[k, i, j])
+
+        median_value = np.median(risk_levels_list)
+        min_value = np.min(risk_levels_list)
+        max_value = np.max(risk_levels_list)
+
+        median_risk_levels[:, i, j] = median_value
+        min_risk_levels[:, i, j] = min_value
+        max_risk_levels[:, i, j] = max_value
+
+    # Load the variable file produced from separate code for variables
+    # Where does selected_variables_by_cluster.nc come from?
+    selected_variables = xr.open_dataset(input_directory + 'p1.nc')
+
+    # Save the min, med, max of risk level, qsnow, acsnom and accprcp.
+    data_save = xr.Dataset(
+        data_vars=dict(
+            risk_min=(["time", "lat", "lon"], min_risk_levels),
+            risk_max=(["time", "lat", "lon"], max_risk_levels),
+            risk_median=(["time", "lat", "lon"], median_risk_levels),
+            risk_all=(["time", "lat", "lon"], risk_levels_copy),
+        ),
+        coords=dict(
+            lat=(["lat"], selected_variables.y.values[::-1],  # flip around the longitundinal axis
+                 {'units': 'crs [m]'}),
+            lon=(["lon"], selected_variables.x.values, {'units': 'crs [m]'}),
+        ),
+        attrs=dict(description="Risk Level"),
+    )
+
+    # save the file
+    data_save.to_netcdf(input_directory + 'preds.nc')
+
+    end = time.time()
+    print("Time: {0:.4f}s".format(end - start))
+
+    s3 = boto3.resource('s3')
+    data_put = open(input_directory+'preds.nc', 'rb')
+
+    s3.Bucket(
+        'aicoe-runoff-risk-netcdfs').put_object(Key="test.nc", Body=data_put)
+    data_put.close()
+
+lambda_handler("a", "b")
---
+++ model/nwm_predict.py
@@ -0,0 +1,295 @@
+#!/usr/bin/env python
+# coding: utf-8
+import datetime as dt
+import warnings
+from copy import deepcopy
+import numpy as np
+from joblib import load
+from joblib import dump
+import glob
+import pandas as pd
+import xarray as xr
+import xgboost as xgb
+import datetime
+import time
+import re
+import os
+import pip
+print(__doc__)
+# importing necessary libraries
+
+warnings.filterwarnings("ignore")
+
+# pip install xgboost==1.6.0
+
+# physical parameters
+geo_vars = [
+    'Lat',
+    'Lon',
+    'albedo12m',
+    'greenfrac',
+    'hgt_m',
+    'landusef',
+    'lu_index',
+    'slopecat',
+    'snoalb',
+    'soilcbot',
+    'soilctop',
+    'soiltemp'
+]
+
+# accumulated variables on a daily scale
+acc_vars = [
+    'SWFORC',
+    'LWFORC',
+    'FSA',
+    'FIRA',
+    'IRB',
+    'QQSFC ',
+    'QQSUB',
+    'SHG',
+    'QSNOW',
+    'RAINRATE'
+]
+
+# average varialbes on a dily scale
+avg_vars = [
+    'TGB',
+    'T2MVM',
+    'Q2MV',
+    'SOIL_M1',
+    'SOIL_M2',
+    'SOIL_M3',
+    'SOIL_M4',
+    'SOIL_T1',
+    'SOIL_T2',
+    'SOIL_T3',
+    'SOIL_T4',
+    'SOIL_W1',
+    'SOIL_W2',
+    'SOIL_W3',
+    'SOIL_W4',
+    'SOILICE',
+    'SOILSATM',
+    'ZWATTABLRT',
+    'SNEQV',
+    'EMISS',
+    'SFCHEADSUBRTM',
+    'SFHD'
+]
+
+# calculate daily values from the accumulated ones
+diff_vars = ['ACCPRCP', 'ACSNOM', 'QBDRYRT', 'QSNOW']
+
+# variables
+land_vars = [
+    'SWFORC', 'LWFORC', 'ACCPRCP', 'EMISS', 'FSA', 'FIRA', 'HFX', 'LH', 'EDIR', 'ETRAN', 'ZWT', 'WA',
+    'RAINRATE', 'WT', 'TR', 'IRG', 'SHG', 'EVG', 'SAG', 'IRB', 'SHB', 'EVB', 'TRAD', 'TG', 'TGV', 'TGB', 'T2MVM',
+    'Q2MV', 'Q2MB', 'ZSNSO_SN', 'SNICE', 'SNLIQ', 'SOIL_T1', 'SNOW_T', 'SNOWH', 'SNEQV', 'QSNOW',
+    'ISNOW', 'FSNO', 'ACSNOW', 'ACSNOM', 'CM', 'CH', 'CHV', 'CHB', 'CHLEAF', 'CHUC', 'CHV2', 'CHB2',
+    'RTMASS', 'STMASS', 'WOOD', 'NEE', 'GPP', 'ACCET', 'SOILICE', 'SOILSATM', 'SNOWT_AVG', 'SOIL_T2',
+    'SOIL_T3', 'SOIL_T4', 'SOIL_WM_1', 'SOIL_WM_2', 'SOIL_WM_3', 'SOIL_WM_4'
+]
+
+runoff_vars = [
+    'QBDRYRT',
+    'SOIL_MM_1',
+    'SOIL_MM_2',
+    'SOIL_MM_3',
+    'SOIL_MM_4',
+    'SFCHEADSUBRTM',
+    'SFHD',
+    'ZWATTABLRT',
+    'QQSFC ',
+    'qqsub_acc'
+]
+
+
+def save_data_to_netCDF(inputs, predict_results):
+    # save Xarray to NetCDF
+    predict_results.time.attrs['unit'] = 'Time days'
+    # print(predict_results)
+    c = predict_results.transpose('time', 'y', 'x')
+    # save xarray to netCDF
+    c.to_netcdf(inputs)
+
+
+def extract_date(file_path: str):
+    file_name = file_path.split('/')[-1]
+    match = re.search("(\d{8})", file_name)
+    date_str = match.group(1)
+    return pd.to_datetime(date_str, format='%Y%m%d')
+
+
+def get_daily_values(land_output, runoff_output, variables, inf_land_var, inf_runoff_var):
+    new_data_sets = {}
+
+    # Concatenate the datasets along the time dimension
+    inland = [x for x in land_output if 'RT' not in x]
+    inroute = [x for x in runoff_output if 'RT' in x]
+
+    ds_list1 = [xr.open_dataset(file) for file in inland]
+    ncland = xr.concat(ds_list1, dim='time')
+    time_vals = [extract_date(file) for file in inland]
+    ncland['time'] = xr.DataArray(time_vals, dims='time')
+
+    ds_list2 = [xr.open_dataset(file) for file in inroute]
+    ncroute = xr.concat(ds_list2, dim='time')
+    time_vals = [extract_date(file) for file in inroute]
+    ncroute['time'] = xr.DataArray(time_vals, dims='time')
+
+    soil_layers = [0, 1, 2, 3]
+    for layer in soil_layers:
+        new_var = ncroute['SOIL_MM'].sel(soil_layers_stag=layer)
+        new_var = new_var.rename('SOIL_MM_' + str(layer + 1))
+        ncroute = xr.merge([ncroute, new_var])
+
+    for layer in soil_layers:
+        new_var = ncland['SOIL_WM'].sel(soil_layers_stag=layer)
+        new_var = new_var.rename('SOIL_WM_' + str(layer + 1))
+        if 'y_2' in new_var.dims:
+            new_var = new_var.rename({'y_2': 'y'})
+        ncland = xr.merge([ncland, new_var])
+
+    new_data_sets = xr.Dataset()
+
+    rain_rate = np.array(ncland['RAINRATE'].values)
+
+    for var in variables:
+
+        ############################
+        ###### Scenario 1 ##########
+        if var in inf_land_var:
+            subset = ncland[var]
+            if var == 'RAINRATE':
+                subset = ncland[var] * 3600
+
+            if var == 'QSNOW':
+                subset = ncland[var] * 3600
+
+            if np.argwhere(np.isnan(subset.values)).any():
+                print('%s has NaN values.' % var)
+
+            else:
+                subset = ncland[var]
+
+        elif var in inf_runoff_var:
+            subset = ncroute[var]
+
+            if np.argwhere(np.isnan(subset.values)).any():
+                print('%s has NaN values.' % var)
+
+        else:
+            print('%s not found in either ncland or ncroute.' % var)
+
+        new_data_sets = xr.merge([subset, new_data_sets], join="override")
+    print(new_data_sets)
+
+    rain_rate_2 = np.array(ncland['RAINRATE'].values)
+
+    print("rain_rate == rain_rate_2: ", np.allclose(rain_rate, rain_rate_2))
+
+    if np.isclose(rain_rate, rain_rate_2, rtol=1e-9, atol=1e-9).all():
+        print("The two arrays are equal.")
+    else:
+        print("The two arrays are not equal.")
+    return new_data_sets
+
+
+# Get the hourly values of influential variables from Model Inputs
+def get_daily_values_from_nwm(land_output, runoff_output, variables):
+    # Land/routing file
+    # Split influential variables into the land and runoff variables
+    inf_land_var = []
+    inf_runoff_var = []
+
+    for var in variables:
+        if var in land_vars:
+            inf_land_var.append(var)
+        elif var in runoff_vars:
+            inf_runoff_var.append(var)
+        elif var.lower() in {'zwattablrt', 'sfhd', 'qqsfc_acc', 'qqsub_acc'}:
+            inf_runoff_var.append(var)
+        else:
+            print(
+                'Influential variable: {0:s} is not included in model output'.format(var))
+            exit()
+
+    # Get daily values of influential variables
+    daily_values = get_daily_values(
+        land_output, runoff_output, variables, inf_land_var, inf_runoff_var)
+
+    return daily_values
+
+
+def predict_runoff(file_path, datasets, variables):
+    # check if all variables are in datasets
+    datasets_df = datasets.to_dataframe()
+    colnames = datasets_df.columns.to_list()
+
+    # print(datasets_df[variables[::-1]])
+
+    if not all(col in colnames for col in variables):
+        print('not all variables appear in the data set')
+        exit()
+
+    # load the classification model - predict occurrence and its probabilty of the daily EOF runoff
+    occ_mod_name = file_path + "clas.joblib.dat"
+    cv_occ_mod = load(occ_mod_name)
+    # print(cv_occ_mod)
+
+    # data for occurrence prediction
+    val_c_xv = datasets_df[variables[::-1]]
+
+    # print(val_c_xv)
+    # print(np.where(val_c_xv.values > 0))
+    # print(val_c_xv.values[570][0])
+
+    # predict the occurrence of the EOF events (0 or 1) using the trained XGBoost classification trees
+    val_c_yv_ev = cv_occ_mod.predict(val_c_xv)
+
+    # predict the occurrence probability of the EOF events [0-1] (the probability is the values of the 1st column)
+    val_c_yv_mod = cv_occ_mod.predict_proba(val_c_xv)[:, 1]
+    # val_c_yv_mod = cv_occ_mod.predict_proba(val_c_xv)
+
+    # load the regression model - predict runoff magnitude
+    reg_mod_name = file_path + 'reg.joblib.dat'
+    cv_reg_mod = load(reg_mod_name)
+
+    # data for magnitude prediction of daily EOF events; Note that the same data is the same as the one used for the occurrence prediction
+    val_m_xv = datasets_df[variables[::-1]]
+    # print(val_m_xv)
+
+    # predict runoff magnitude using the trained XGBoost regression trees
+    val_m_yv_mod = cv_reg_mod.predict(val_m_xv)
+
+    # Event: 0/1 for runoff occurrence; PROB: [0, 1] for occurence probability; RUNOFF: >=0 for the magnitudes of runoff
+
+    # get the order of dimensions in the datasets object
+    order = list(datasets.dims.keys())
+
+    # determine the shape of each variable based on the order of dimensions
+    event_shape = [datasets.dims[dim] for dim in order]
+    prob_shape = [datasets.dims[dim] for dim in order]
+    mag_shape = [datasets.dims[dim] for dim in order]
+
+    # reshape the variables based on the order of dimensions
+    runoff_event = xr.DataArray(
+        data=val_c_yv_ev.reshape(event_shape),
+        dims=order, coords=datasets.coords, name='EVENT')
+
+    runoff_prob = xr.DataArray(
+        data=val_c_yv_mod.reshape(prob_shape),
+        dims=order, coords=datasets.coords, name='PROB')
+
+    runoff_mag = xr.DataArray(
+        data=val_m_yv_mod.reshape(mag_shape),
+        dims=order, coords=datasets.coords, name='RUNOFF')
+
+    # save the results
+    res_runoff = xr.merge(
+        [runoff_event, runoff_mag, runoff_prob], join="override")
+    # print(res_runoff)
+
+    # res_runoff.rename_vars({'rainrate':'ACCPRCP', 'sfhd':'SFCHEADSUBRT'})
+    return res_runoff
